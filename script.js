866dd1313f1de2dd19bb0467b8fd159ce47a77ef
Stephanie Hickman <stephick@gmail.com>
2024-01-20 15:26:13 -0600

    additional currency tweaks

--- script.js
+++ script.js
@@ -25,6 +25,24 @@ function addEmployee(event){
   document.getElementById('title').value = "";
   document.getElementById('annualSalary').value = "";
 
+
+/* Create our number formatter.  Got this information from
+  https://stackoverflow.com/questions/149055/how-to-format-numbers-as-currency-strings */
+  
+const formatter = new Intl.NumberFormat('en-US', {
+  style: 'currency',
+  currency: 'USD',
+  minimumFractionDigits: 0
+
+  // These options are needed to round to whole numbers if that's what you want.
+  //minimumFractionDigits: 0, // (this suffices for whole numbers, but will print 2500.10 as $2,500.1)
+  //maximumFractionDigits: 0, // (causes 2500.99 to be printed as $2,501)
+});
+
+let currencyStringAnnualSalary = formatter.format(annualSalary);
+console.log ('Formatted annual salary:', currencyStringAnnualSalary);
+console.log(typeof annualSalary);
+
   
   // Adding employee row to existing table element when submit is clicked
   tableElement.innerHTML += `
@@ -33,31 +51,32 @@ function addEmployee(event){
     <td>${lastName}</td>
     <td>${id}</td>
     <td>${title}</td>
-    <td>${annualSalary}</td>
+    <td>${currencyStringAnnualSalary}</td>
     <td>
       <button onclick="deleteRow(event)">Delete</button>
     </td>
   </tr>
 `;
-  
+ 
 
-totalMonthlyCost += Number(annualSalary)/12;
-console.log ('Annual salary', annualSalary);
-console.log ('Current value of total monthly cost', totalMonthlyCost);
+totalMonthlyCost += ((Number(annualSalary))/12);
+//console.log ('Current value of total monthly cost', totalMonthlyCost);
+
 totalMonthlyCost = Number(totalMonthlyCost);
-totalMonthlyCost = Math.round(totalMonthlyCost*100)/100;
+
 console.log('Total Monthly cost:', totalMonthlyCost);
 
 
 
-document.querySelector('footer').innerHTML = `Total Monthly: $${totalMonthlyCost}`;
+/* Display total monthly cost in USD format */
+document.querySelector('footer').innerHTML = `Total Monthly: ${formatter.format(totalMonthlyCost)}`;
 
-// if the total monthly cost exceedds $20,000, we need to indicate this
-  //by changing the appearance of the footer
+/* if the total monthly cost exceedds $20,000, we need to indicate this
+    by changing the appearance of the footer */
 
     if(totalMonthlyCost > 20000){
       let footer = document.querySelector('footer');
-      console.log(footer);
+      //console.log(footer);
       footer.classList.add('over-budget');
   }
   else{
